1ebba77863f77fb3073f5f6a8afc75d04db9fbc3
Jordyn Vermut <jvermut@umich.edu>
2023-11-12 14:25:07 -0500

    nothing rlly

--- List.hpp
+++ List.hpp
@@ -249,7 +249,6 @@ void List<T>::pop_back() {
 
 template <typename T>
 void List<T>::clear() {
-  //assert(!empty()); //while {!empty()}
   for (int i = 0; i < size(); ++i) {
     pop_front();
   }
