0f5c8b8cec8bbc04a2678837ac5143b02997fec3
idesousa <idesousa@umich.edu>
2023-11-11 23:24:04 -0500

    added tests

--- List_tests.cpp
+++ List_tests.cpp
@@ -15,4 +15,22 @@ TEST(test_stub) {
     ASSERT_TRUE(list1.empty());
 }
 
+TEST (test_empty_list) {
+List<int> myList;
+ASSERT_TRUE(myList.empty());
+}
+
+TEST (test_not_empty_list) {
+List<int> myList; myList.push_back(1);
+ASSERT_FALSE (myList .empty() )
+}
+
+TEST (test_empty_list_pop_front) {
+List<int> myList; myList.push_back(1); myList.push_back(2); myList.push_back(3); myList.push_back(4); myList.push_back (5);
+int size = mylist.size h
+for(int i = 0; i < size; i++)
+myList.pop_ front O
+ASSERT_TRUE (myList.empty())
+}
+
 TEST_MAIN()
