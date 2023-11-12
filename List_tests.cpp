b42f053a1ca00582a5ea3036a73c2d6db329b455
idesousa <idesousa@umich.edu>
2023-11-11 22:43:56 -0500

    one test

--- List_tests.cpp
+++ List_tests.cpp
@@ -7,7 +7,12 @@ using namespace std;
 
 TEST(test_stub) {
     // Add test code here
-    ASSERT_TRUE(true);
+    List <int> list1;
+    list1.push_back(3);
+    list1.push_back(2);
+    list1.push_back(1);
+    list1.clear();
+    ASSERT_TRUE(list1.empty());
 }
 
 TEST_MAIN()
